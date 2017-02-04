c4e78ad0fad94579012c00ea55144baae0f7f1c8
Eriko <lo_oleriko@hotmail.com>
2017-02-04 04:34:39 +0000

    add status

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,6 +8,8 @@
     
         {!! Form::label('content','タスク:') !!}
         {!! Form::text('content') !!}
+        {!! Form::label('status','ステータス:') !!}
+        {!! Form::text('status') !!}
         {!! Form::submit('投稿') !!}
         
     {!! Form::close() !!}
